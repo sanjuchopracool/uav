0585e6f42376afe947fd97d94523a2345204816c
sanjuchopracool <sanjuchopracool@gmail.com>
2012-11-17 17:06:51 +0530

    done

--- App/moc_SerialApp.cpp
+++ App/moc_SerialApp.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'SerialApp.h'
 **
-** Created: Sat Nov 17 11:58:14 2012
+** Created: Sat Nov 17 16:55:35 2012
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.3)
 **
 ** WARNING! All changes made in this file will be lost!
